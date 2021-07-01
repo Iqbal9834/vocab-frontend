50a815b8e14c1530acfb946c5b9c515cea17bb2f
iqbal_ali8 <iqbalali9554@gmail.com>
2021-07-01 14:55:38 +0530

    commit

--- src/component/homeComponent.js
+++ src/component/homeComponent.js
@@ -5,6 +5,7 @@ import Grid from "@material-ui/core/Grid";
 
 import "../App.css";
 import SearchBar from "material-ui-search-bar";
+import IconButton from '@material-ui/core/IconButton';
 
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
